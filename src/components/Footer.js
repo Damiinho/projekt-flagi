b90c54b9db4e623e830305e8e4dbcb7e40336106
Damian Ludef <damiinho@gmail.com>
2023-01-17 18:58:36 +0100

    add current year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../style/Footer.css";
 
-class Header extends React.Component {
+class Footer extends React.Component {
   render() {
-    return <footer>Damian Ludef — 2021</footer>;
+    return <footer>Damian Ludef — 2021–${now.getFullYear()}</footer>;
   }
 }
 
-export default Header;
+export default Footer;
